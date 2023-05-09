fdfb12143580546ce764849d39d0dc148e20e470
Reynaldo Endis <reynaldo.endis@gmail.com>
2023-05-09 03:58:11 -0400

    01 basic example

---
+++ example/doc_registry.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rendis/statepro/piece"
+)
+
+type DogMachineDefinitions[T Dog] struct{}
+
+func (DogMachineDefinitions[T]) GetMachineTemplateId() string {
+	return "DogMachine"
+}
+
+// action -> (dog Dog, evt piece.Event, actTool piece.ActionTool[Dog]) error
+// predicate -> (dog Dog, evt Event) (bool, error)
+// invocation -> (dog Dog, evt Event) ServiceResponse
+
+func (DogMachineDefinitions[T]) NotifySleeping(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog is sleeping")
+	return nil
+}
+
+// 01 - Basic example
+func (DogMachineDefinitions[T]) IncreaseEnergy(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog is increasing energy")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) NotifyMovement(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog is moving")
+	return nil
+}
+
+// 01 - Basic example
+func (DogMachineDefinitions[T]) StartEating(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog is eating")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) StopEating(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog stopped eating")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) StartPlaying(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog is playing")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) StopPlaying(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog stopped playing")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) DecreaseEnergy(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog is decreasing energy")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) StartInvestigating(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog is investigating")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) StopInvestigating(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog stopped investigating")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) LoseInterest(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog lost interest")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) StartSniffing(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog is sniffing")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) StopSniffing(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog stopped sniffing")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) Investigate(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog is investigating")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) Alert(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) A loud noise was heard. Dog is alert")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) HaveADream(_ Dog, _ piece.Event, _ piece.ActionTool[Dog]) error {
+	fmt.Println("- (A) Dog is dreaming with a bone")
+	return nil
+}
+
+func (DogMachineDefinitions[T]) IsLoudNoise(_ Dog, _ piece.Event) (bool, error) {
+	response := true
+	fmt.Printf("- (G) Check if noise is loud. Noise is loud: %v\n", response)
+	return response, nil
+}
+
+func (DogMachineDefinitions[T]) IsEnergyLow(_ Dog, _ piece.Event) (bool, error) {
+	response := true
+	fmt.Printf("- (G) Check if energy is low. Energy is low: %v\n", response)
+	return response, nil
+}
---
+++ example/dog_machine_without_invk.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rendis/statepro"
+	"github.com/rendis/statepro/piece"
+	"strings"
+)
+
+func runDogMachineExamples() {
+	// ------- 01-example-basic -----
+	//initDogMachine()
+
+	//_, _ = getDogMachine(nil)
+
+	//getDogMachineInfo()
+
+	//dogMachineSendEvent()
+
+	//initDogMachineOnState()
+
+	//selfEventBehavior()
+}
+
+// show how to register a machine and init all machines
+func initDogMachine() {
+	definitions := &DogMachineDefinitions[Dog]{}
+	_ = statepro.AddMachine[Dog](definitions)
+	statepro.InitMachines()
+	fmt.Println("DogMachine registered and initialized")
+}
+
+// show how to get a machine by id
+func getDogMachine(dog *Dog) (string, piece.ProMachine[Dog]) {
+	definitions := &DogMachineDefinitions[Dog]{}
+	dogMachineId := statepro.AddMachine[Dog](definitions)
+	statepro.InitMachines()
+
+	if dog == nil {
+		dog = &Dog{}
+	}
+	dogMachine, err := statepro.GetMachineById[Dog](dogMachineId, dog)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\nDogMachineId: %s\n\n", dogMachineId)
+
+	return dogMachineId, dogMachine
+}
+
+// show how to get machine info
+func getDogMachineInfo() {
+	dog := &Dog{}
+	_, dogMachine := getDogMachine(dog)
+
+	nextEvents := dogMachine.GetNextEvents()
+	fmt.Printf("DogMachine next events: %v\n", nextEvents)
+
+	isFinalState := dogMachine.IsFinalState()
+	fmt.Printf("DogMachine is final state: %v\n", isFinalState)
+
+	currentState := dogMachine.GetState()
+	fmt.Printf("DogMachine current state: %s\n", currentState)
+}
+
+// show how to build and send an event
+func dogMachineSendEvent() {
+	dog := &Dog{}
+	_, dogMachine := getDogMachine(dog)
+
+	evt := piece.BuildEvent("NOISE").Build()
+	_ = dogMachine.SendEvent(evt)
+
+	printMachineInfo(dogMachine)
+}
+
+// show how to init a machine on a specific state
+func initDogMachineOnState() {
+	dog := &Dog{}
+	_, dogMachine := getDogMachine(dog)
+
+	initOnState := "Awake"
+	_ = dogMachine.PlaceOn(initOnState)
+
+	printMachineInfo(dogMachine)
+
+	evt := piece.BuildEvent("TIRED").Build()
+	_ = dogMachine.SendEvent(evt)
+
+	printMachineInfo(dogMachine)
+}
+
+// show it-self event behavior
+func selfEventBehavior() {
+	dog := &Dog{}
+	_, dogMachine := getDogMachine(dog)
+
+	evt := piece.BuildEvent("DREAM").Build()
+	_ = dogMachine.SendEvent(evt)
+
+	printMachineInfo(dogMachine)
+}
+
+func printMachineInfo(machine piece.ProMachine[Dog]) {
+	fmt.Println("=========================================")
+
+	currentState := machine.GetState()
+	fmt.Printf("* DogMachine current state: %s\n", currentState)
+
+	isFinalState := machine.IsFinalState()
+	fmt.Printf("* DogMachine is final state: %v\n", isFinalState)
+
+	nextEvents := machine.GetNextEvents()
+	// join next events by comma
+	nextEventsStr := strings.Join(nextEvents, ", ")
+	fmt.Printf("* DogMachine next events: %v\n", nextEventsStr)
+	fmt.Println("=========================================")
+}
