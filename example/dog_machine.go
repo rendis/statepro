ab02399914b7bb03d2b23591bbef06236783a23c
Reynaldo Endis <reynaldo.endis@gmail.com>
2023-05-10 21:48:47 -0400

    05 invocations services

--- example/dog_machine.go
+++ example/dog_machine.go
@@ -12,6 +12,7 @@ func runDogMachineExamples() {
 	/*************** DOG MACHINE **************/
 	/* Uncomment the example you want to run */
 	/*******************************************/
+	statepro.SetDefinitionPath("example/statepro.yml")
 
 	invokeServices()
 }
