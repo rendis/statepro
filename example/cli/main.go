fbb999d3d006180fba67009183711bfb84227a32
rendis <reynaldo.endis@gmail.com>
2024-06-28 13:22:59 -0400

    cli example

--- example/cli/main.go
+++ example/cli/main.go
@@ -10,15 +10,63 @@ import (
 func main() {
 	registerLaws()
 	debugger := cli.NewStateMachineDebugger()
-	debugger.SetStateMachinePath("example/cli/state_machine.json")
-	debugger.SetEventsPath("example/cli/events.json")
+	debugger.SetStateMachinePath("./state_machine.json")
+	debugger.SetEventsPath("./events.json")
+	debugger.SetSnapshotsPath("./snapshots.json")
 	debugger.Run(nil)
 }
 
 func registerLaws() {
-	_ = builtin.RegisterAction("disableAdmission", DisableAdmission)
+	_ = builtin.RegisterAction("action:disableAdmission", disableAdmission)
+	_ = builtin.RegisterAction("action:createForm", createForm)
+	_ = builtin.RegisterAction("action:sendContract", sendContract)
+	_ = builtin.RegisterCondition("condition:ifItIsTheTemplate", ifItIsTheTemplate)
 }
 
-func DisableAdmission(_ context.Context, _ instrumentation.ActionExecutorArgs) error {
+func disableAdmission(_ context.Context, _ instrumentation.ActionExecutorArgs) error {
+	return nil
+}
+
+func ifItIsTheTemplate(_ context.Context, args instrumentation.ConditionExecutorArgs) (bool, error) {
+	// get templateId from args
+	templateId, ok := getTemplateIdFromArg(args.GetCondition().Args)
+	if !ok {
+		return false, nil
+	}
+
+	// get templateId from event
+	eventTemplateIdAny, ok := args.GetEvent().GetData()["templateId"]
+	if !ok {
+		return false, nil
+	}
+
+	fromTemplateId, ok := eventTemplateIdAny.(string)
+	if !ok {
+		return false, nil
+	}
+
+	isTheTemplate := templateId == fromTemplateId
+	if !isTheTemplate {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func getTemplateIdFromArg(args map[string]any) (string, bool) {
+	templateIdAny, ok := args["templateId"]
+	if !ok {
+		return "", false
+	}
+
+	templateId, ok := templateIdAny.(string)
+	return templateId, ok
+}
+
+func createForm(_ context.Context, _ instrumentation.ActionExecutorArgs) error {
+	return nil
+}
+
+func sendContract(_ context.Context, _ instrumentation.ActionExecutorArgs) error {
 	return nil
 }
